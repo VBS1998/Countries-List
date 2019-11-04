c969d48f0fc75e53457c32c164edf9bfaba53518
Gustavo Vilas Boas <gustavovbs98@gmail.com>
2019-11-04 12:45:43 -0300

    Arquivos para scene da lista de paises adicionada

---
+++ Countries List/Countries List/CountriesListScene/CountriesListInteractor.swift
@@ -0,0 +1,13 @@
+// Generated using Sourcery 0.17.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+// TODO: Remove this Sourcery mark above
+import UIKit
+
+protocol CountriesListInteractorDelegate {
+}
+
+final class CountriesListInteractor: CountriesListInteractorProtocol {
+    var presenter : CountriesListPresenter?
+}
+
---
+++ Countries List/Countries List/CountriesListScene/CountriesListViewController.swift
@@ -0,0 +1,21 @@
+// Generated using Sourcery 0.17.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+// TODO: Remove this Sourcery mark above
+import UIKit
+
+protocol CountriesListViewControllerDelegate: AnyObject {
+
+}
+
+final class CountriesListViewController: CoordenableViewController {
+
+    var coordinator : CountriesListCoordinator = CountriesListCoordinator()
+    var interactor : CountriesListInteractor!
+    override func viewDidLoad() {
+        interactor = CountriesListInteractor()
+        interactor.presenter = CountriesListPresenter()
+        interactor.presenter?.viewController = self
+    }
+
+}
